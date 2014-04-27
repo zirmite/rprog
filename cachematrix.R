53ccfddb1c59a4b7bf8c95a2bd81180a2d196ff1
Andrew Zirm <azirm@dark-cosmology.dk>
2014-04-27 20:17:35 +0200

    initial edits

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) m <<- inv
+        getinv <- function() inv
+        list(set = set, get = get,
+             setmean = setinv,
+             getmean = getinv)
 }
 
 
